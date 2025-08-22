c2f2ed422d6d3bd486a2c261367011c87d401314
Thomas Childs <tgc243@icloud.com>
2025-08-22 21:01:30 +0100

    set up and tested database migrations

--- app/lib/schema.ts
+++ app/lib/schema.ts
@@ -1,37 +1,42 @@
-import { pgTable, varchar, uuid, integer, date } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, uuid, integer, date, timestamp, text, unique } from 'drizzle-orm/pg-core';
 import { sql } from 'drizzle-orm';
 import { relations } from 'drizzle-orm';
 
 // Users table
 export const users = pgTable('users', {
-  id: uuid('id').primaryKey().default(sql`uuid_generate_v4()`),
-  name: varchar('name', { length: 255 }).notNull(),
-  email: varchar('email').notNull().unique(),
-  password: varchar('password').notNull(),
-});
+  id: uuid().default(sql`uuid_generate_v4()`).primaryKey().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  email: text().notNull(),
+  password: text().notNull(),
+}, (table) => [
+  unique("users_email_key").on(table.email),
+]);
 
 // Customers table  
 export const customers = pgTable('customers', {
-  id: uuid('id').primaryKey().default(sql`uuid_generate_v4()`),
-  name: varchar('name', { length: 255 }).notNull(),
-  email: varchar('email', { length: 255 }).notNull(),
-  imageUrl: varchar('image_url', { length: 255 }).notNull(),
+  id: uuid().default(sql`uuid_generate_v4()`).primaryKey().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull(),
+  imageUrl: varchar("image_url", { length: 255 }).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
 // Invoices table
 export const invoices = pgTable('invoices', {
-  id: uuid('id').primaryKey().default(sql`uuid_generate_v4()`),
-  customerId: uuid('customer_id').notNull().references(() => customers.id),
-  amount: integer('amount').notNull(),
-  status: varchar('status', { length: 255 }).notNull(),
-  date: date('date').notNull(),
+  id: uuid().default(sql`uuid_generate_v4()`).primaryKey().notNull(),
+  customerId: uuid("customer_id").notNull().references(() => customers.id),
+  amount: integer().notNull(),
+  status: varchar({ length: 255 }).notNull(),
+  date: date().notNull(),
 });
 
 // Revenue table
 export const revenue = pgTable('revenue', {
-  month: varchar('month', { length: 4 }).primaryKey(),
-  revenue: integer('revenue').notNull(),
-});
+  month: varchar({ length: 4 }).notNull(),
+  revenue: integer().notNull(),
+}, (table) => [
+  unique("revenue_month_key").on(table.month),
+]);
 
 // Define relations
 export const customersRelations = relations(customers, ({ many }) => ({
